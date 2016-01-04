61e8c25bf08d454beb22b8a95d7cfe2dbd395e8c
Daniel Caaz <DCaazy@gmail.com>
2016-01-04 09:47:20 -0800

    Moved interactivity out of here

--- js/flow.js
+++ js/flow.js
@@ -20,37 +20,5 @@ function Flow($contentParent, $navParent, obj) {
   $navParent.append($nav);
   // Same for content!
   $contentParent.append($section);
-
   // Now that all the page stuff is handled, let's make some interactivity!
-
-  // viewing is the id link thing that we're viewing!
-  var viewing = 0;
-  // This updates our navigation's links to have a special class for fancying up with css!
-  var updateNav = function() {
-    // Grab all the navigation's children!
-    $nav.children()
-    // Remove their flow-viewing class!
-    .removeClass('flow-viewing')
-    // Now get the child with the viewing id!
-    .eq(viewing)
-    // Add in that flow-viewing class!
-    .addClass('flow-viewing');
-  }
-  // Now let's update our navigation to have it asap!
-  updateNav();
-  // Let's throw in a scroll handler to make all that defining useful!
-  $(window).scroll(function(e) {
-    var scroll = $(window).scrollTop();
-    var height = $(window).height();
-    // Divide our scroll amount by height getting a value that should correspond with our navigation!
-    // Unless there's other shit on the page. I didn't account for that. Hm.
-    // Maybe I could use navParent? For scrolling that'd work.... There's all sorts of issues that could happen with this.
-    var at = Math.floor(scroll/height);
-    // If our at doesn't equal viewing that means we've switched what we're looking at
-    if(at != viewing) {
-      viewing = at;
-      // So update our navigation.
-      updateNav();
-    }
-  });
 }
